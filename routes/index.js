d3f1bd5b0349289c226ebf3cb7dfd9d488f411a6
Chaim Solomon <chaim.solomon@naturalint.com>
2016-11-17 08:44:55 +0200

    Update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var requestIp = require('request-ip');
+var connect = require('connect');
+var app = connect();
+app.use(requestIp.mw());
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { ip_address: req.clientIp });
 });
 
 module.exports = router;
